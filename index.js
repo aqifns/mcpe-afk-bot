1813c26eb4ed1ede3e91449feea1f8e0d9402bf1
aqifns <mohdaqifaqif26@gmail.com>
2025-04-27 22:31:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const { createClient } = require('minecraft-protocol');
 
+const client = createClient({
+  host: "your-aternos-server.aternos.me", // change this
+  port: 19132,
+  username: "AFK_Bot_123",
+  version: "1.19.62"
+});
+
+client.on('connect', () => {
+  console.log('Bot connected!');
+});
+
+client.on('disconnect', (reason) => {
+  console.log('Bot disconnected:', reason);
+});
+
+client.on('error', (err) => {
+  console.log('Bot error:', err);
+});
